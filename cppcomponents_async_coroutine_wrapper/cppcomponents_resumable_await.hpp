59514b225f2e1c6e2088b9f1edf4e4a919566d1b
John Bandela <jbandela@gmail.com>
2013-08-30 17:34:22 -0400

    Added resumable/await support

---
+++ cppcomponents_async_coroutine_wrapper/cppcomponents_resumable_await.hpp
@@ -0,0 +1,208 @@
+#include <cppcomponents/future.hpp>
+#include "cppcomponents_async_coroutine_wrapper.hpp"
+
+// Copyright (c) 2013 John R. Bandela
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+#ifndef ASYNC_HELPER_PPL_HELPER_IMP_INCLUDED_04_24_2013
+#define ASYNC_HELPER_PPL_HELPER_IMP_INCLUDED_04_24_2013
+
+#include <memory>
+#include <exception>
+#include <utility>
+#include <assert.h>
+#include <type_traits>
+#include <functional>
+
+#ifdef PPL_HELPER_OUTPUT_ENTER_EXIT
+#include <cstdio>
+#include <atomic>
+#define PPL_HELPER_ENTER_EXIT ::CPP_ASYNC_AWAIT_PPL_NAMESPACE::detail::EnterExit ppl_helper_enter_exit_var;
+#else
+#define PPL_HELPER_ENTER_EXIT
+#endif
+
+namespace cppcomponents{
+	namespace detail{
+#ifdef PPL_HELPER_OUTPUT_ENTER_EXIT
+		// Used for debugging to make sure all functions are exiting correctly
+		struct EnterExit{
+			EnterExit() : n_(++number()){ increment(); std::printf("==%d Entering\n", n_); }
+			int n_;
+			std::string s_;
+			static int& number(){ static int number = 0; return number; }
+			static std::atomic<int>& counter(){ static std::atomic<int> counter_ = 0; return counter_; }
+			static void increment(){ ++counter(); }
+			static void decrement(){ --counter(); }
+			static void check_all_destroyed(){ assert(counter() == 0); if (counter())throw std::exception("Not all EnterExit destroyed"); };
+			~EnterExit(){ decrement(); std::printf("==%d Exiting\n", n_); }
+
+		};
+#endif
+#undef PPL_HELPER_OUTPUT_ENTER_EXIT
+
+
+		struct coroutine_holder : std::enable_shared_from_this<coroutine_holder>{
+			PPL_HELPER_ENTER_EXIT;
+			typedef cppcomponents_async_coroutine_wrapper::CoroutineVoidPtr co_type;
+			std::unique_ptr<co_type> coroutine_;
+			co_type::CallerType* coroutine_caller_;
+			coroutine_holder() : coroutine_(), coroutine_caller_(nullptr){}
+
+		};
+
+		struct ret_type{
+			std::exception_ptr eptr_;
+			void* pv_;
+
+			template<class T>
+			T& get(){
+				if (eptr_){
+					std::rethrow_exception(eptr_);
+				}
+				return *static_cast<T*>(pv_);
+			}
+		};
+
+		struct convertible_to_async_helper{};
+
+
+	}
+
+	template<class T>
+	class awaiter{
+		typedef detail::coroutine_holder* co_ptr;
+		typedef std::function<use<IFuture<T>>()> func_type;
+		co_ptr co_;
+	public:
+		awaiter(co_ptr c)
+			: co_(c)
+		{
+
+		}
+
+
+
+		awaiter(detail::convertible_to_async_helper);
+
+
+		template<class R>
+		R operator()(use<IFuture<R>> t){
+			PPL_HELPER_ENTER_EXIT;
+			assert(co_->coroutine_caller_);
+			auto co = co_;
+			func_type retfunc([co, t]()mutable{
+				auto sptr = co->shared_from_this();
+				return t.Then([sptr](use<IFuture<R>> et)mutable{
+					detail::ret_type ret;
+					ret.eptr_ = nullptr;
+					ret.pv_ = nullptr;
+					ret.pv_ = &et;
+					(*sptr->coroutine_)(&ret);
+					try{
+						func_type f(std::move(*static_cast<func_type*>(sptr->coroutine_->Get())));
+						return f();
+					}
+					catch (std::exception&){
+						ret.eptr_ = std::current_exception();
+						ret.pv_ = nullptr;
+						(*sptr->coroutine_)(&ret);
+						throw;
+					}
+				}).Unwrap();
+			});
+
+			(*co_->coroutine_caller_)(&retfunc);
+			return static_cast<detail::ret_type*>(co_->coroutine_caller_->Get())->get<use<IFuture<R>>>().Get();
+		}
+
+	};
+	namespace detail{
+
+		template<class T>
+		struct ret_holder{
+			T value_;
+			template<class FT>
+			ret_holder(FT& f, awaiter<T> h) : value_(f(h)){}
+			const T& get()const{ return value_; }
+
+			use<IFuture<T>> get_ready_future()const{ return cppcomponents::make_ready_future(value_); }
+		};
+		template<>
+		struct ret_holder<void>{
+			template<class FT>
+			ret_holder(FT& f, awaiter<void> h){
+				f(h);
+			}
+			void get()const{}
+			use<IFuture<void>> get_ready_future()const{ return cppcomponents::make_ready_future(); }
+
+		};
+
+		template<class F>
+		class simple_async_function_holder : public coroutine_holder{
+
+
+			F f_;
+			typedef typename std::result_of<F(convertible_to_async_helper)>::type return_type;
+			typedef use<IFuture<return_type>> task_t;
+			typedef std::function<task_t()> func_type;
+
+
+			static void coroutine_function(cppcomponents::use<cppcomponents_async_coroutine_wrapper::ICoroutineVoidPtr> ca){
+				PPL_HELPER_ENTER_EXIT;;
+
+				auto p = ca.Get();
+				auto pthis = reinterpret_cast<simple_async_function_holder*>(p);
+				pthis->coroutine_caller_ = &ca;
+				try{
+					PPL_HELPER_ENTER_EXIT;
+					awaiter<return_type> helper(pthis);
+					ret_holder<return_type> ret(pthis->f_, helper);
+					func_type retfunc([ret](){
+						return ret.get_ready_future();
+					});
+					ca(&retfunc);
+				}
+				catch (std::exception&){
+					auto eptr = std::current_exception();
+					func_type retfunc([eptr](){
+						task_t ret;
+						std::rethrow_exception(eptr);
+						return ret;
+					});
+					ca(&retfunc);
+				}
+			}
+		public:
+			simple_async_function_holder(F f) : f_(f){}
+
+			task_t run(){
+				coroutine_.reset(new coroutine_holder::co_type(cppcomponents::make_delegate<cppcomponents_async_coroutine_wrapper::CoroutineHandler>(coroutine_function), this));
+				func_type f(std::move(*static_cast<func_type*>(coroutine_->Get())));
+				return f();
+
+			}
+		};
+
+		template<class F>
+		struct return_helper{};
+
+		template<class R>
+		struct return_helper<R(awaiter<R>)>{
+			typedef R type;
+		};
+	}
+
+	template<class F>
+	use<IFuture<typename std::result_of<F(detail::convertible_to_async_helper)>::type>> resumable(F f){
+		auto ret = std::make_shared<detail::simple_async_function_holder<F>>(f);
+		return ret->run();
+	}
+}
+
+
+#endif
--- testing/unit_test_exe.cpp
+++ testing/unit_test_exe.cpp
@@ -3,10 +3,86 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 #include <string>
-#include "../cppcomponents_async_coroutine_wrapper/cppcomponents_async_coroutine_wrapper.hpp"
+#include "../cppcomponents_async_coroutine_wrapper/cppcomponents_resumable_await.hpp"
 #define CATCH_CONFIG_MAIN  
 #include "external/catch.hpp"
 
+struct ICounter : public cppcomponents::define_interface < cppcomponents::uuid<0x35779857, 0x9090, 0x4668, 0x8eee, 0xa67e359b66ae>>
+{
+	std::uint64_t Increment();
+	std::uint64_t Decrement();
+	std::uint64_t Count();
+
+	CPPCOMPONENTS_CONSTRUCT(ICounter, Increment, Decrement, Count);
+};
+
+
+
+inline std::string launch_on_new_thread_executor_id(){ return "launch_on_new_thread"; }
+typedef cppcomponents::runtime_class < launch_on_new_thread_executor_id, cppcomponents::object_interfaces<cppcomponents::IExecutor, ICounter>,
+	cppcomponents::factory_interface < cppcomponents::NoConstructorFactoryInterface >> launch_on_new_thread_executor_t;
+
+struct launch_on_new_thread_executor : public cppcomponents::implement_runtime_class<launch_on_new_thread_executor, launch_on_new_thread_executor_t>{
+	typedef cppcomponents::delegate < void() > ClosureType;
+	std::atomic<std::uint64_t> count_;
+	launch_on_new_thread_executor() : count_{ 0 }{}
+
+	struct counter_helper{
+
+		cppcomponents::use<ICounter> c_;
+		bool success_;
+
+		counter_helper(cppcomponents::use<ICounter> c)
+			: c_{ c }, success_{ false }
+		{
+			c_.Increment();
+		}
+
+		void success(){
+			success_ = true;
+		}
+
+		~counter_helper(){
+			if (!success_){
+				c_.Decrement();
+			}
+		}
+
+	};
+
+	void AddDelegate(cppcomponents::use<ClosureType> d){
+		auto counter = this->QueryInterface<ICounter>();
+
+		counter_helper helper(counter);
+
+		std::thread t(
+			[d, counter](){
+				d();
+				counter.Decrement();
+		});
+
+		helper.success();
+
+		if (t.joinable()){
+			t.detach();
+		}
+
+	}
+	std::size_t NumPendingClosures(){
+		return static_cast<std::size_t>(ICounter_Count());
+	}
+
+	std::uint64_t ICounter_Increment(){
+		return count_.fetch_add(1);
+	}
+	std::uint64_t ICounter_Decrement(){
+		return count_.fetch_sub(1);
+	}
+	std::uint64_t ICounter_Count(){
+		return count_.load();
+	}
+
+};
 
 
 
@@ -37,3 +113,27 @@ TEST_CASE("Test wrapper", "[coroutine]") {
 	REQUIRE(i2 == 5);
 }
 
+
+TEST_CASE("Async", "[async]"){
+
+	std::atomic<bool> done{false};
+
+	int i = 0;
+
+	auto f = cppcomponents::resumable([&done, &i](cppcomponents::awaiter<void> await){
+
+		auto f = cppcomponents::async(launch_on_new_thread_executor::create().QueryInterface<cppcomponents::IExecutor>(),
+			[]{return 10; });
+
+		i = await(f);
+		done.store(true);
+		//return 0;
+
+	});
+
+
+	// busy wait
+	while (done.load() == false);
+
+	REQUIRE(i == 10);
+}
